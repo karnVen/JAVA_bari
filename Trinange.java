2d88a57f176c1d2f202c2ff1197241eceaad4daf
karnVen <onemonth06@gmail.com>
2026-03-03 22:09:52 +0530

    feat: Lecture 0 to 5.2

---
+++ Trinange.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+
+public class Trinange{
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Float base = Float.valueOf(scanner.nextline());
+    }
+}
