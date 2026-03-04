bffdca758ab195f19d801eb091b88a9c9b255b05
karnVen <onemonth06@gmail.com>
2026-03-04 23:48:30 +0530

    triangel program

--- Trinange.java
+++ Trinange.java
@@ -4,5 +4,7 @@ public class Trinange{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Float base = Float.valueOf(scanner.nextline());
+        float high = float.valueOf(scanner.nextline());
+
     }
 }
