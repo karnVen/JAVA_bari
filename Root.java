7b3e5d2948b05185210b1b519f88fe07b4f03fd0
karnVen <onemonth06@gmail.com>
2026-03-08 14:08:53 +0530

    feat: 5.4(31L)_QuadraticEquation

---
+++ Root.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Root{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        double r1=((-b)+Math.sqrt((b*b)-(4*a*c)))/2*a;
+        double r2=((-b)-Math.sqrt((b*b)-(4*a*c)))/2*a;
+
+        System.out.println(r1);
+        System.out.println(r2);
+    
+    }
+}
