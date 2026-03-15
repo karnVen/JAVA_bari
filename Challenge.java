681123b4498f8da455615306ec18b5bf1a835090
karnVen <onemonth06@gmail.com>
2026-03-15 09:21:18 +0530

    feat: till 7.5_conditions

---
+++ Challenge.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+class Challenge{
+    public static void main(String[] args) {
+        //1
+       
+        //2nd
+        // int a =10011;
+        // String str3=a+"";
+        // System.out.println(str3.matches("[01]*"));
+        // String b ="2398ABCD";
+        // System.out.println(b.matches("[123456789ABCDEF]+"));
+        //4TH
+        // 
+        //5th
+        // String str= "avf     adfd      adf  ";
+        // String a=str.replaceAll("\\s+"," ");
+        // System.out.println(a)
+        // 
+        //6th
+        Scanner sc = new Scanner(System.in);
+        String a =sc.nextLine();
+        if(a.matches("[01]*")){
+            System.out.println("binary");
+        }else if (a.matches("[0-7]*")){
+            System.out.println("octal");
+        }else if(a.matches("[0-7A-F]*")){
+            System.out.println("hexa");
+        }
+
+
+
+    }
+}
--- StringPra.java
+++ StringPra.java
@@ -1,15 +1,19 @@
 public class StringPra {
 
     public static void main(String[] args) {
-        String str1 ="java";
-        char c[]={'j','d','d'};
-        String str2= new String(c,1,2);
-        System.out.println(str2);
-        System.out.println(str1);
+        // String str1 ="java";
+        // char c[]={'j','d','d'};
+        // String str2= new String(c,1,2);
+        // System.out.println(str2);
+        // System.out.println(str1);
 
-        String str4= new String("java");
-        String str5 ="java";
+        // String str4= new String("java");
+        // String str5 ="java";
 
-        System.out.println(str4==str5);
+        // System.out.println(str4==str5);
+
+        String str6="java";
+        String str7="java";
+        System.out.println(str6==str7);
     }
 }
