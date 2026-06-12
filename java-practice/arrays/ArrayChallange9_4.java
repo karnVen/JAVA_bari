09315d8813de10efa8dc80ff9d16f09981e3f2da
karnVen <onemonth06@gmail.com>
2026-06-12 23:55:06 +0530

    ...

--- java-practice/arrays/ArrayChallange9_4.java
+++ java-practice/arrays/ArrayChallange9_4.java
@@ -1,6 +1,7 @@
 public class ArrayChallange9_4 {
     public static void main(String[] args) {
-        int A[]={1,2,3,4,5,};
+        int A[]=new int[5];
+         A[0]=1;A[1]=2;
         // int temp= A[0];
 
         // for(int i =1; i<A.length; i++){
@@ -13,17 +14,37 @@ public class ArrayChallange9_4 {
         //     System.out.print(x);
         // }
        
-        int last =A[A.length-1];
-        System.out.println(last);
-        for (int i=0; i<A.length;i++){
-           A[i-1]= A[i];
+        // int last =A[A.length-1];
+        // System.out.println(last);
+        // for (int i=0; i<A.length;i++){
+        //    A[i-1]= A[i];
 
            
+        // }
+        // A[0]=last;
+        // for(int Y:A){
+        //     System.out.print(Y);
+        // }
+
+
+        //shifting an element 
+        int x = 5;
+        int index=2;
+        int n =A.length;
+
+        for(int i=n;i>index;i--){
+        A[i]=A[i-1];
+        A[index]=x;
         }
-        A[0]=last;
-        for(int Y:A){
-            System.out.print(Y);
+
+        for(int i = 0;i<n;i++){
+            System.out.println(A[i]+",");
+            System.out.println("");
         }
+            
+
+        
+
 
         
     }
